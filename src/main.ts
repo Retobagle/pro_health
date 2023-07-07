8950dccd6e84c49dfaa1db1662bbdff2839f316a
Retobagle <Retobagle@163.com>
2023-07-07 18:22:17 +0800

    feat: 顶部、底部静态组件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/style/reset.scss'
+import App from '@/App.vue'
+// 引入全局组件顶部、底部
+import HospitalTop from '@/components/hospital_top/index.vue'
+import HospitalBottom from '@/components/hospital_bottom/index.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('HospitalTop', HospitalTop);
+app.component('HospitalBottom', HospitalBottom);
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// 引入node内置模块path，获取绝对路径
+import path from 'path';
+
 export default defineConfig({
   plugins: [vue()],
+  // src文件夹配置别名
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, 'src')
+    }
+  }
 })
